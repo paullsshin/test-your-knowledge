9d6f5c61336d3ecd5b2c7a5312b44be1ef525014
paullsshin <paullsshin@hotmail.com>
2022-12-02 19:16:51 -0800

    working on styling for the game css

---
+++ assets/game.js
@@ -0,0 +1,15 @@
+var head = document.querySelector(".heading");
+var start = document.querySelector("#start-btn");
+var score = document.querySelector("#high-score");
+var wins = document.querySelector("#wins");
+var lose = document.querySelector("#losses");
+var reset = document.querySelector("#reset");
+var time = document.querySelector(".timer");
+var timerText = document.querySelector("#timer-text");
+var questions = [];
+var winCounter = 0;
+var loseCounter = 0;
+var timer;
+var timerCount;
+
+var 
